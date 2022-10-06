8c263ea64a636364d2ab70fe834586537377bec6
alexbaumgertner <alex.baumgertner@gmail.com>
2022-10-06 15:07:46 +0000

    Deploying to gh-pages from  @ a1a82bec4bc06caa1f90cdb445db48d2e0554bb7 🚀

---
+++ _next/static/X1B17IXNbsr5zBnw4A4k2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-74722554c5915d93.js"],"/_error":["static/chunks/pages/_error-fa2da97f07e0d864.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
